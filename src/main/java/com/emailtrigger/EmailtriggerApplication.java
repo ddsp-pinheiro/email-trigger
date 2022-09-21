00a1787ba51c813acf5e9aa0a6aa6adc3d749228
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2022-09-21 14:43:33 -0300

    Initial commit

---
+++ src/main/java/com/emailtrigger/EmailtriggerApplication.java
@@ -0,0 +1,13 @@
+package com.emailtrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailtriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailtriggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emailtrigger/EmailtriggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailtrigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailtriggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
